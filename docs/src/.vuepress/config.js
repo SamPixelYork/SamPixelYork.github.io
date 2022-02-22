9e6099d28ae362b9fad79ac4136c7124c2776051
Sam Pixel York <sampixelyork@protonmail.com>
2022-02-23 10:12:23 +1100

    update base

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -5,6 +5,7 @@ module.exports = {
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
   title: 'Vuepress Docs Boilerplate',
+  base: '/',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
